8641d84b7b2d61ddd5c2b1fd0ee4419de2785ec6
Johan Kjölhede <kjolhede@telia.com>
2016-02-28 01:04:28 +0100

    cleanup

--- src/test/scala/se/gigurra/testmonocle/Companion.scala
+++ src/test/scala/se/gigurra/testmonocle/Companion.scala
@@ -5,32 +5,6 @@ trait Companion[T] {
   def apply() : C
 }
 
-trait CC[T] {
-  type C <: AnyRef
-  def apply() : C
-}
-
 object Companion {
   implicit def getCompanion[T](implicit comp : Companion[T]) = comp()
 }
-
-/*
-object TestCompanion {
-trait Foo
-
-object Foo {
-  def bar = "wibble"
-
-  // Per-companion boilerplate for access via implicit resolution
-  implicit def companion = new Companion[Foo] {
-    type C = Foo.type
-    def apply() = Foo
-  }
-}
-
-import Companion._
-
-val fc = companion[Foo]  // Type is Foo.type
-val s = fc.bar           // bar is accessible
-}
-*/
